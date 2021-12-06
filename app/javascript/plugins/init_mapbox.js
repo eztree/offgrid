c39896f7a6f1a8dbfbc65482919e14f29840381f
eztree <huienlim@gmail.com>
2021-12-06 15:01:18 +0800

    Geocoder set up

---
+++ app/javascript/plugins/init_mapbox.js
@@ -0,0 +1,19 @@
+import mapboxgl from 'mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+
+const initMapbox = () => {
+  const mapElement = document.getElementById('map');
+
+  if (mapElement) { // only build a map if there's a div#map to inject into
+    mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
+    const map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v10'
+    });
+    map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
+      mapboxgl: mapboxgl }));
+  }
+};
+
+export { initMapbox };
