c703d0aa32762208f0381ca94ecba6ee2ec84ad9
eztree <huienlim@gmail.com>
2021-12-06 18:03:22 +0800

    Trips model created

---
+++ db/migrate/20211206100224_create_trips.rb
@@ -0,0 +1,18 @@
+class CreateTrips < ActiveRecord::Migration[6.1]
+  def change
+    create_table :trips do |t|
+      t.references :user, null: false, foreign_key: true
+      t.date :start_date
+      t.date :end_date
+      t.integer :no_of_people
+      t.references :trail, null: false, foreign_key: true
+      t.string :status
+      t.boolean :cooking
+      t.boolean :camping
+      t.string :last_seen_photo
+      t.datetime :release_date_time
+
+      t.timestamps
+    end
+  end
+end
