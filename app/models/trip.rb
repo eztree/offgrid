c330cc59fe2267b2743044a763ba9b657afc7b2a
jensen yuen <jensenyuen@outlook.com>
2021-12-07 15:30:25 +0800

    added validation for trip

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,14 @@
 class Trip < ApplicationRecord
   belongs_to :user
   belongs_to :trail
+
+  validates :start_date, presence: false
+  validates :end_date, presence: false
+  validates :no_of_people, presence: false
+  validates :status, presence: false
+  validates :cooking, presence: false
+  validates :camping, presence: false
+  validates :last_seen_photo, presence: false
+  validates :release_date_time, presence: false
+
 end
