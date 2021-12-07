6d0d40c6cb1dc1d9819b88182b8bbe0b9b8c1810
jensen yuen <jensenyuen@outlook.com>
2021-12-07 13:16:27 +0800

    added new route for trips

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :trails, only: [ :index, :show ]
+  resources :trails, only: [ :index, :show ] do
+  end
+  resources :trips, only: [ :new, :create ]
   resources :steps
 end
