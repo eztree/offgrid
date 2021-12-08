dd467802e3d61f82904e6cd5b1f12d9a6e5d7298
jensen yuen <jensenyuen@outlook.com>
2021-12-08 12:14:46 +0800

    renamed methods to match pages

--- app/controllers/steps_controller.rb
+++ app/controllers/steps_controller.rb
@@ -2,9 +2,10 @@ class StepsController < ApplicationController
   skip_before_action :authenticate_user!
   skip_after_action :verify_authorized
   before_action :assign_trip, only: [:show, :update]
+  before_action :assign_emergency_contact, only: [:show, :update]
 
   include Wicked::Wizard
-  steps :add_date_and_people, :add_options, :add_emergency_contact
+  steps :date_people, :options, :emergency_contact
 
   def show
     render_wizard
@@ -13,16 +14,19 @@ class StepsController < ApplicationController
   def update
     @user = find_user
     case step
-    when :add_date_and_people
+    when :date_people
       if trips_params
         @trip.start_date = trips_params[:start_date]
         @trip.no_of_people = trips_params[:no_of_people]
       end
-    when :add_options
+    when :options
       if trips_params
         @trip.camping = trips_params[:camping]
         @trip.cooking = trips_params[:cooking]
+        @trip.save
       end
+    when :emergency_contact
+      raise
     end
     render_wizard @trip
   end
@@ -40,6 +44,11 @@ class StepsController < ApplicationController
 
   def find_user
     @user = current_user.nil? ? @trip.user : current_user
+  end
+
+  def assign_emergency_contact
+    find_user
+    @emergency_contact = @user.emergency_contacts.present? ? @user.emergency_contacts : EmergencyContact.new
   end
 
 end
