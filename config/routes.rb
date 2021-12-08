2964a13f679cc87e995556fc023c103c0956c6a9
jensen yuen <jensenyuen@outlook.com>
2021-12-08 12:15:04 +0800

    added new routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :users, only: [ :show ] do
+    resources :emergency_contact, only: [ :new, :create ]
+  end
   root to: 'pages#home'
   resources :trails, only: [ :index, :show ]
   resources :trips, only: [ :new, :create ] do
