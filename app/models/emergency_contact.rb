6141ec16e06fb0f803e592fd59e55196a88130fe
jensen yuen <jensenyuen@outlook.com>
2021-12-08 18:33:41 +0800

    changed validation

--- app/models/emergency_contact.rb
+++ app/models/emergency_contact.rb
@@ -2,7 +2,7 @@ class EmergencyContact < ApplicationRecord
   belongs_to :user
   has_many :trips
 
-  validates :name, presence: true
-  validates :email, presence: true
-  validates :phone_no, presence: true
+  validates :name, presence: false
+  validates :email, presence: false
+  validates :phone_no, presence: false
 end
