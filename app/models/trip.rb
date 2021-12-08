5552746924d63ad50160ebf2e2371b1684a8e309
jensen yuen <jensenyuen@outlook.com>
2021-12-08 17:18:52 +0800

    changed relations

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,8 +1,8 @@
 class Trip < ApplicationRecord
   belongs_to :user
   belongs_to :trail
+  belongs_to :emergency_contact
   has_many :checklists, dependent: :destroy
-  has_many :safety_records, dependent: :destroy
 
   validates :start_date, presence: true, on: :update
   validates :end_date, presence: false
@@ -12,5 +12,5 @@ class Trip < ApplicationRecord
   validates :camping, presence: false, on: :update
   validates :last_seen_photo, presence: false
   validates :release_date_time, presence: false
-
+  validates :emergency_contact, presence: false
 end
