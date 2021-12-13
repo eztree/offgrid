b8f29e5d75c4b64e04ba9dbf73eaf49acbf84f01
Sufian Suni <sufiansuni@gmail.com>
2021-12-13 16:15:23 +0800

    trails have photo urls

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_12_035012) do
+ActiveRecord::Schema.define(version: 2021_12_13_065815) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -119,6 +119,7 @@ ActiveRecord::Schema.define(version: 2021_12_12_035012) do
     t.string "route_distance"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "photo_url"
   end
 
   create_table "trips", force: :cascade do |t|
