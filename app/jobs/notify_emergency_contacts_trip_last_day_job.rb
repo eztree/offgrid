164f6e70c96566c9edbf51fe04afc55b4803e235
Sufian Suni <sufiansuni@gmail.com>
2021-12-15 15:05:54 +0800

    case corrections

--- app/jobs/notify_emergency_contacts_trip_last_day_job.rb
+++ app/jobs/notify_emergency_contacts_trip_last_day_job.rb
@@ -6,7 +6,7 @@ class NotifyEmergencyContactsTripLastDayJob < ApplicationJob
     trip = Trip.find(trip_id)
     # message to the user
     TwilioTextMessenger.new(
-      message: "OffGrid - #{trip.user.first_name} #{trip.user.last_name} is expected to return from #{trip.trail.location} (#{trip.trail.name}) today. Ensure his/her return and contact relevant authorities in case of emergencies.",
+      message: "Offgrid - #{trip.user.first_name} #{trip.user.last_name} is expected to return from #{trip.trail.location} (#{trip.trail.name}) today. Ensure his/her return and contact relevant authorities in case of emergencies.",
       receiver: trip.emergency_contact.phone_no
     ).call
   end
